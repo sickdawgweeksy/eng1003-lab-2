b54c1f4a34043b25c18a92464e36caa94275d1e5
sickdawgweeksy <timmyweeks@icloud.com>
2020-03-03 13:52:52 +1100

    fixed it where y coordinate wasnt calculating properly Signed-off-by: sickdawgweeksy <timmyweeks@icloud.com>

--- lab2mandle/main.c
+++ lab2mandle/main.c
@@ -2,30 +2,37 @@
 #include <stdlib.h>
 
 int main()
-{ float x0, y0, xn, yn;
+{ float x0, y0, xn, yn, xtemp;
   int count, counter;
 
   counter=0;
 
-    printf("Enter complex coordinate coefficients to test\n");
-    scanf("%f %f", &x0, &y0);
+    printf("Welcome to the Mandlebrot Set coordinate tester\nHere we test if your complex coordinate is in the Mandlebrot Set\n");
+    printf("Please input your real number coordinate that is NOT the coeeficient of i or if you would prefer the a in a+bi: ");
+    scanf("%f", &x0);
+    printf("Please input your real number coordinate that IS the coeeficient of i or if you would prefer the b in a+bi: ");
+    scanf("%f", &y0);
     printf("Input number of iterations: ");
     scanf("%d", &count);
     xn = x0;
     yn = y0;
     while (((xn*xn)+(yn*yn) < 4) && (counter < count)){
 
-    xn = ((xn*xn)-(yn*yn)) +x0;
-    yn = 2*xn*yn+y0;
+    xtemp = ((xn*xn)-(yn*yn)) +x0;
+    yn = (2*xn*yn)+y0; // the problem was the y is calculating with the new coordinate rather than the old one
+    xn = xtemp;
+
+    printf("%f\n", xn);
+    printf("%f\n", yn);
 
     counter = counter +1;
     }
         if (counter == count){
-            printf("congratulations the complex number is in the Mandelbrot set");
+            printf("congratulations the complex number %f + %fi is included in the Mandlebrot Set up to %d iterations", x0, y0, count);
             return 0;
         }
         else{
-            printf("Unfortunately that number is not in the Mandelbrot set");
+            printf("Unfortunately that number is not in the Mandelbrot set, the cordinate %f+%fi failed on the %d iteration when it escaped the radius of 2", x0, y0, counter);
         }
         return 0;
 
